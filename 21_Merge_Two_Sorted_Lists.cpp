edbf432d1a1dc76d9fa3c4239556c3a44e6c0c7c
Asik8 <motasembillah84@gmail.com>
2024-07-06 09:29:58 +0600

    12th done

---
+++ 21_Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL && l2 == NULL)
+            return l1;
+        if (!l1)
+            return l2;
+        if (!l2)
+            return l1;
+        ListNode* head = NULL;
+        ListNode* tail = NULL;
+
+        if (l1->val < l2->val) {
+            head = l1;
+            l1 = l1->next;
+        } else {
+            head = l2;
+            l2 = l2->next;
+        }
+        tail = head;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+        if(l1) tail->next = l1;
+        if(l2) tail->next = l2;
+        return head;
+    }
+};
